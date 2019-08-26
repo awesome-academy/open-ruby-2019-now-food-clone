5cab09fb0b332d0c6f6176da9170eb87bba3bda2
Bayka1997 <nguyenvanbay.ptit@gmail.com>
2019-08-26 15:11:19 +0700

    master layout admin

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,9 @@
+//= require jquery3
+//= require rails-ujs
+//= require activestorage
+//= require popper
+//= require bootstrap-sprockets
+//= require admin/custom.min.js
+//= require admin/perfect-scrollbar.jquery.min.js
+//= require admin/sidebarmenu.js
+//= require admin/waves.js
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root to: "home_pages#index"
 
   devise_for :users
+
+  scope module: "admin" do
+    get "admin/home", to: "homes#index"
+  end
 end
