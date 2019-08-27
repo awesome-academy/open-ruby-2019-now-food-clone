b2eb83c238b1854af43b165723a0e79e45c8baa7
Bayka1997 <nguyenvanbay.ptit@gmail.com>
2019-08-27 08:07:41 +0700

    register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
   
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   private
+  
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
   def default_url_options
     {locale: I18n.locale}
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit :sign_up, keys: %i(name)
+    devise_parameter_sanitizer.permit :account_update, keys: %i(name role)
   end
 end
